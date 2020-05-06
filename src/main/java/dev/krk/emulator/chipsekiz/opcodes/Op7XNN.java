25ed292a4e6f36cde7b38457199286afb2afd661
krk <keremkat@gmail.com>
2020-05-06 23:31:33 +0200

    Introduce PXNN abstract opcode.

--- src/main/java/dev/krk/emulator/chipsekiz/opcodes/Op7XNN.java
+++ src/main/java/dev/krk/emulator/chipsekiz/opcodes/Op7XNN.java
@@ -1,28 +1,8 @@
 package dev.krk.emulator.chipsekiz.opcodes;
 
-import java.util.Optional;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Strings.padStart;
-
-public class Op7XNN extends Opcode {
+public class Op7XNN extends OpPXNN {
     public Op7XNN(int vx, int imm) {
-        super(Optional.of(vx), Optional.empty(), Optional.of(imm));
-        checkArgument(imm == (imm & 0xFF), "immediate out of bounds");
-        checkArgument(vx >= 0 && vx <= 0xF, "register index out of bounds");
-    }
-
-    public int vx() {
-        return super.getVx().get();
-    }
-
-    public byte imm() {
-        return super.getAddress().get().byteValue();
-    }
-
-    @Override public String toString() {
-        return "7" + Integer.toHexString(vx()) + padStart(Integer.toHexString(imm()).toUpperCase(),
-            2, '0');
+        super('7', vx, imm);
     }
 }
-
---
+++ src/main/java/dev/krk/emulator/chipsekiz/opcodes/OpPXNN.java
@@ -0,0 +1,32 @@
+package dev.krk.emulator.chipsekiz.opcodes;
+
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Strings.padStart;
+
+public abstract class OpPXNN extends Opcode {
+    private final char p;
+
+    public OpPXNN(char p, int vx, int imm) {
+        super(Optional.of(vx), Optional.empty(), Optional.of(imm));
+        checkArgument(imm == (imm & 0xFF), "immediate out of bounds");
+        checkArgument(vx >= 0 && vx <= 0xF, "register index out of bounds");
+
+        this.p = p;
+    }
+
+    public int vx() {
+        return super.getVx().get();
+    }
+
+    public byte imm() {
+        return super.getAddress().get().byteValue();
+    }
+
+    @Override public String toString() {
+        return p + Integer.toHexString(vx()) + padStart(Integer.toHexString(imm()).toUpperCase(), 2,
+            '0');
+    }
+}
+
