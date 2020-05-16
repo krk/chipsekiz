af63c2f1e1dbdc65ea90f34f4847317a2857d826
krk <keremkat@gmail.com>
2020-05-16 15:19:07 +0200

    Add chipsekiz-root and empty emulator project.

---
+++ emulator-awt/src/test/java/dev/krk/chipsekiz/AppTest.java
@@ -0,0 +1,20 @@
+package dev.krk.chipsekiz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
